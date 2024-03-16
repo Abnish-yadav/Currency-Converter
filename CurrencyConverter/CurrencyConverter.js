3dcf5673ec80b46eccd8190ea2b8f1c9c6025b56
Abnish_ydv <111004389+Abnish-yadav@users.noreply.github.com>
2024-03-16 15:34:29 +0530

    Add files via upload
    here three currency coverter uesd

---
+++ CurrencyConverter/CurrencyConverter.js
@@ -0,0 +1,17 @@
+function convert() {
+    const amount = document.getElementById("amount").value;
+    const fromCurrency = document.getElementById("fromCurrency").value;
+    const toCurrency = document.getElementById("toCurrency").value;
+
+    fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`)
+        .then(response => response.json())
+        .then(data => {
+            const exchangeRate = data.rates[toCurrency];
+            const convertedAmount = (amount * exchangeRate).toFixed(2);
+            document.getElementById("result").innerText = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            document.getElementById("result").innerText = "Error occurred while fetching data";
+        });
+}
